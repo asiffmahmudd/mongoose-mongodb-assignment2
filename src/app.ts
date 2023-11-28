bcc88d14efb959bbc936534d87f529800618f961
Asif Mahmud <asifmahmud3472@gmail.com>
2023-11-28 12:54:41 -0500

    added userroutes on app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { UserRoutes } from "./app/modules/user.route";
 const app: Application = express();
 
 //parsers
@@ -7,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-// app.use("/api/v1/students", StudentRoutes);
+app.use("/api/v1/users", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
 
